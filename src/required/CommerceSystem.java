fdcea33f94deea86539d9a2222f1e98dd9d113bf
b7749abc92-boop <b7749abc92@gmail.com>
2026-06-22 14:37:37 +0900

    커머스 과제 진행중_2

--- src/required/CommerceSystem.java
+++ src/required/CommerceSystem.java
@@ -1,4 +1,34 @@
 package required;
 
 public class CommerceSystem {
+    Scanner sc = new Scanner(System.in);
+    private List<Product> prod;
+
+
+    public CommerceSystem(List<Product> prod3) {
+        this.prod = prod3;
+    }
+    public void start() {
+
+
+        System.out.println("전자제품 상품 목록");
+        int i = 1;
+        for (Product p : prod) {
+
+            System.out.println(i + ". " + p.getname() + "    │   " + p.getpri() + "   │   " + p.getdescrip());
+            i++;
+        }
+        System.out.println("0. 종료       │   프로그램 종료");
+
+        String s = sc.next();
+        if ("0".equals(s)) {
+            System.out.println("프로그램 종료");
+            sc.close();
+            System.exit(0);
+        }
+
+    }
+
+
+
 }
--- src/required/Main.java
+++ src/required/Main.java
@@ -1,11 +1,10 @@
 package required;
 
 public class Main {
-
-    public static List<Product> prod = new ArrayList<>();
-
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        List<Product> prod3=new ArrayList<>();
+
+
         Product pr1 = new Product("Galaxy S24", 1200000, "최신 스마트폰", 50);
         Product pr2 = new Product("Galaxy Tab S10", 340000, "고해상도 디스플레이를 지원하는 태블릿", 24);
         Product pr3 = new Product("Galaxy Book Pro", 1000000, "휴대성이 뛰어난 노트북", 15);
@@ -14,31 +13,18 @@ public class Main {
         Product pr6 = new Product("Galaxy Buds Pro", 100000, "노이즈 캔슬링 기능을 제공하는 무선 이어폰", 36);
 
 
-        prod.add(pr1);
-        prod.add(pr2);
-        prod.add(pr3);
-        prod.add(pr4);
-        prod.add(pr5);
-        prod.add(pr6);
+        prod3.add(pr1);
+        prod3.add(pr2);
+        prod3.add(pr3);
+        prod3.add(pr4);
+        prod3.add(pr5);
+        prod3.add(pr6);
+        CommerceSystem cs = new CommerceSystem(prod3);
 
-        System.out.println("전자제품 상품 목록");
-
-        int i = 1;
-        for (Product p : prod) {
+        cs.start();
 
 
 
-            System.out.println(i + ". " + p.getname() + "    │   " + p.getpri() + "   │   " + p.getdescrip());
-            i++;
-        }
-
-        System.out.println("0. 종료       │   프로그램 종료");
-        String s = sc.next();
-        if ("0".equals(s)) {
-            System.out.println("프로그램 종료");
-            sc.close();
-            System.exit(0);
-        }
 
 
 
